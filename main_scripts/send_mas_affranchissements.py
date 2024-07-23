1b41abbf0d86de89b11a3ab94c509c220d8368b0
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-07-23 17:19:52 +0200

    supression du message d'erreur

--- main_scripts/send_mas_affranchissements.py
+++ main_scripts/send_mas_affranchissements.py
@@ -4,7 +4,6 @@ from utils.mas_csv_processing import read_and_clean_mas_csv, rename_mas_colums, 
 
 def send_mas_affranchissements(file_path: str):
     if not file_is_correct(get_src_path_to_filename(file_path)):
-        print(f"incorrect file detected : {file_path}")
         return
     cleaned_mas = read_and_clean_mas_csv(file_path)
     cleaned_mas = rename_mas_colums(cleaned_mas)
