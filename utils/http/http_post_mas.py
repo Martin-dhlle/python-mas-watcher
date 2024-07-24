417af57f192de2a34f189e187c8dd390c52736e1
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-07-24 13:56:41 +0200

    gestion de la réponse de la requête

--- utils/http/http_post_mas.py
+++ utils/http/http_post_mas.py
@@ -1,10 +1,16 @@
-from http.client import HTTPSConnection
+import requests
 
-from config.request_config import API
-from utils.models.affranchissement_mas import AffranchissementMas
+from config.request_config import API, API_MAS_KEY
 
-def http_post_mas(json_str: str):
-    headers = {'Content-Type': 'application/json'}
-    connection = HTTPSConnection(API)
-    connection.request('POST', url='/mas', headers=headers, body=json_str)
-    return
+def http_post_mas(json_str: str, file_path: str):
+    '''
+    Requête en post afin de créer des affranchissements MAS
+    dans la base de données
+    '''
+    headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {API_MAS_KEY}'}
+    response = requests.post(API, headers=headers, data=json_str)
+    
+    if response.ok():
+        print(f"Les données du fichier d'affranchissements MAS {file_path} ont bien été sauvegardés")
+    else:
+        print(f"erreur status code : {response.status_code}")
