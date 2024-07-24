88f6eec7b8972124d37bb637594af94fff8703eb
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-07-24 11:12:19 +0200

    intégration requête api

--- main_scripts/send_mas_affranchissements.py
+++ main_scripts/send_mas_affranchissements.py
@@ -1,5 +1,6 @@
 from utils.check_file import file_is_correct, get_src_path_to_filename
 from utils.mas_csv_processing import read_and_clean_mas_csv, rename_mas_colums, convert_df_to_json_obj
+from utils.http.http_post_mas import http_post_mas
 
 
 def send_mas_affranchissements(file_path: str):
@@ -11,7 +12,6 @@ def send_mas_affranchissements(file_path: str):
         return
     cleaned_mas = read_and_clean_mas_csv(file_path)
     cleaned_mas = rename_mas_colums(cleaned_mas)
-    json_mas = convert_df_to_json_obj(cleaned_mas)
+    affranchissements_mas = convert_df_to_json_obj(cleaned_mas)
 
-    # test logs
-    print(json_mas[0])
+    http_post_mas(affranchissements_mas)
--- utils/http/http_post_mas.py
+++ utils/http/http_post_mas.py
@@ -1,8 +1,9 @@
 from http.client import HTTPSConnection
 
 from config.request import API
+from utils.models.affranchissement_mas import AffranchissementMas
 
-def http_post_mas():
+def http_post_mas(affranchissements_mas: list[AffranchissementMas]):
     connection = HTTPSConnection(API)
     connection.request('POST', url='/mas', headers=[""])
     return
