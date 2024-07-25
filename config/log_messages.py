498a4355930c3aeef4e745e29c39782c44879873
Martin Dhollande <mdhollande@fabriquenumerique.fr>
2024-07-25 17:16:52 +0200

    lowercase + guillements enlevés

--- config/log_messages.py
+++ config/log_messages.py
@@ -1,7 +1,7 @@
 LOG_MESSAGES= [
     ["nouveau fichier", "Nouveau fichier détecté: {arg}"],
     ["attente fichier", "En attente de nouveau fichier dans le répertoire : {arg}"],
-    ["variables manquantes", "Variables d'environnement {arg} manquantes"],
+    ["variables manquantes", "Variables d'environnement {arg} manquantes dans le fichier .env.local"],
     ["donnees sauvegardes", "Les données du fichier d'affranchissements MAS {arg} ont bien été sauvegardés"],
     ["erreur requete", "erreur status code : {arg}"],
     ["erreur validation", "Validation failed on {arg} expected."]
